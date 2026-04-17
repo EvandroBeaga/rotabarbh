e8fd40ff10494f99ed00bca1ad825e2db957b3f2
EvandroBeaga <evandrobeaga47@gmail.com>
2026-04-17 09:17:52 -0300

    Add files via upload

---
+++ dados_bares.js
@@ -0,0 +1,7 @@
+const baresBH = [
+    { nome: "222", lat: -19.94578, lng: -43.92848, prato: "Cumbuca de Carne de Sol", end: "R. Francisco Deslandes, 222" },
+    { nome: "Alexandre's Bar", lat: -19.86312, lng: -43.95462, prato: "Petisco do Alexandre", end: "R. David Alves do Vale, 68" },
+    { nome: "Andrade's Beer", lat: -19.79634, lng: -44.00456, prato: "Sabor Mineiro", end: "R. Dona Geni, 32" },
+    { nome: "Armazém Santa Amélia", lat: -19.85123, lng: -43.98741, prato: "Costelinha Embriagada", end: "Rua da Sinfonia, 253" },
+    // A lista completa com os 127 eu te envio em blocos assim que este rodar!
+];
